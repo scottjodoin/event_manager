8638d41b4decc51f59a53d78014e9103ea62faea
Scott Jodoin <scottjodoin@gmail.com>
2020-05-05 15:27:17 -0500

    dev: add form letter Signed-off-by: Scott Jodoin <scottjodoin@gmail.com>

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,16 +1,46 @@
-class EventManager
-  require "csv"
-  puts "EventManager Initialized!"
+require "csv"
+require "google/apis/civicinfo_v2"
+require 'erb'
 
-  contents = CSV.open "./event_attendees.csv", headers: true, header_converters: :symbol
-  contents.each do |row|
-  name = row[:first_name]
-  zipcode = row[:zipcode]
-  puts "#{name} #{zipcode}"
+class EventManager
+  CivicInfo = Google::Apis::CivicinfoV2
+  @civic_info = CivicInfo::CivicInfoService.new
+  @civic_info.key = File.read("../keys/civic_api.key").strip
+
+  def legislators_by_zipcode(zipcode)
+    begin
+      legislators = @civic_info.representative_info_by_address(
+        address: zipcode,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      ).officials
+    rescue
+      "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+    end
   end
 
   def self.clean_zip_code (zipcode)
-    puts test
     zipcode.to_s.rjust(5,"0")[0..4]
   end
+
+  puts "EventManager Initialized!"
+
+  def do_stuff
+    contents = CSV.open "./event_attendees.csv", headers: true, header_converters: :symbol
+    template_letter = File.read "./form_letter.html"
+    erb_template = ERB.new template_letter
+
+    contents.each do |row|
+      name = row[:first_name]
+
+      zipcode = row[:zipcode]
+
+      legislators = legislators_by_zipcode(zipcode)
+
+      puts form_letter = erb_template.result(binding)
+    end
+  end
 end
+
+em =  EventManager.new
+em.do_stuff
